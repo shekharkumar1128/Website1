9725885ab2c5a2ae40baade34c2c428dd86f32be
shekharkumar1128 <shekharkumar1128@gmail.com>
2024-09-19 14:52:54 +0530

    E-Commerce Website

---
+++ models/products.js
@@ -0,0 +1,106 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tags : [
+        {
+            type : String,
+        }
+    ],
+    images: [
+        {
+            type: String,
+            required: true
+        }
+    ],
+    videos:[
+        {
+            type:String,
+        }
+    ],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    oldPrice: {
+        type: Number,
+        default: 0
+    },
+    catName:{
+        type:String,
+        default:''
+    },
+    catId:{
+        type:String,
+        default:''
+    },
+    subCatId:{
+        type:String,
+        default:''
+    },
+    subCat:{
+        type:String,
+        default:''
+    },
+    subCatName:{
+        type:String,
+        default:''
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    discount: {
+        type: Number,
+        required: true,
+    },
+    productWeight: [
+        {
+            type: String,
+            default: null,
+        }
+    ],
+    location:{
+        type:String,
+        default:"All"
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+
+productSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+productSchema.set('toJSON', {
+    virtuals: true,
+});
+
+exports.Product = mongoose.model('Product', productSchema);
